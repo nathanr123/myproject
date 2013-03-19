06e2b82b3eb9ccf37d37dadf28ed6e712424ade3
nathanr123 <nathanr_kamal@kamalanathan>
2013-03-19 10:24:50 +0530

    Committed in another Place

--- src/mypackage/test/Test.java
+++ src/mypackage/test/Test.java
@@ -1,11 +1,20 @@
 package mypackage.test;
 
-public class Test {
+import javax.swing.JFrame;
+
+public class Test extends JFrame {
+
+	public Test() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(10, 10, 500, 500);
+		setVisible(true);
+	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		new Test();
 		System.out.println("Tested - OK");
 
 	}
