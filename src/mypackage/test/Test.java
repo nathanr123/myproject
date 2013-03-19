f16d0339fd81ebb4693f23073be3ec0c2d6cfcc1
nathanr123 <nathanr_kamal@kamalanathan>
2013-03-19 10:56:35 +0530

    fd

--- src/mypackage/test/Test.java
+++ src/mypackage/test/Test.java
@@ -4,7 +4,14 @@ import javax.swing.JFrame;
 
 public class Test extends JFrame {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
 	public Test() {
+		
+		setTitle("Testing");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(10, 10, 500, 500);
 		setVisible(true);
